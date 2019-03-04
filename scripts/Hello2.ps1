e88bfb5a87c96a3c518159f983282d495a1edb57
Michael Garrison <mgarrison93@gmail.com>
2019-03-04 15:50:58 -0500

    Add files via upload

---
+++ scripts/Hello2.ps1
@@ -0,0 +1,6 @@
+﻿#@Title: Hello World 2
+#@Author: Michael Garrison
+#@Published: 3/4/19
+#@Description: Display the following message: "Hello World!  This is the second script!"
+
+Write-Host "Hello World!  This is the second Script!"
---
+++ scripts/Hello3.ps1
@@ -0,0 +1,6 @@
+﻿#@Title: Hello World 3
+#@Author: Michael Garrison
+#@Published: 3/4/19
+#@Description: Display the following message: "Hello World!  This is the third script!"
+
+Write-Host "Hello World!  This is the third Script!"
